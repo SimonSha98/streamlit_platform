b0ee5694eec373ca941ddc383ea2d398b6147408
SimonSha98 <99288453+SimonSha98@users.noreply.github.com>
2022-07-29 18:15:33 -0400

    Update config.py

--- wsb/config.py
+++ wsb/config.py
@@ -1,6 +1,6 @@
 API_URL = 'https://paper-api.alpaca.markets'
-API_KEY = 'PKL1RGXWJDJ7Z9NNI1EV'
-API_SECRET = 'XISajHmPiXjGDzEaYVMJoQqkddryWy55lOaorGLn'
+API_KEY = ''
+API_SECRET = ''
 
 DB_HOST = '127.0.0.1'
 DB_USER = 'root'
